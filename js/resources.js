224b11992bf94778cf43eda1f08fdb92f275ad34
obiot <olivier.biot@gmx.com>
2013-04-28 02:20:13 -0300

    Renamed `resources` to  `data` (shorted) and fixed the example in the `ressource.js` file

--- js/resources.js
+++ js/resources.js
@@ -2,28 +2,27 @@ game.resources = {
 
 	/* Graphics. 
 	 * @example
-	 * {name: "example", type:"image", src: "resources/img/example.png"},
+	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
-	
-	/* Maps. 
-	 * @example
-	 * {name: "example01", type: "tmx", src: "data/example01.tmx"},
-	 * {name: "example01", type: "tmx", src: "data/example01.json"},
- 	 */
-
 	
 	/* Atlases 
 	 * @example
-	 * {name: "example_tps", type: "tps", src: "data/gfx/example_tps.json"},
+	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
 	 */
-	
+		
+	/* Maps. 
+	 * @example
+	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+ 	 */
+
 	/* Background music. 
 	 * @example
-	 * {name: "example_bgm", type: "audio", src: "data/audio/", channel : 1},
+	 * {name: "example_bgm", type: "audio", src: "data/bgm/", channel : 1},
 	 */	
 	
 	/* Sound effects. 
 	 * @example
-	 * {name: "example_sfx", type: "audio", src: "data/audio/", channel : 2}
+	 * {name: "example_sfx", type: "audio", src: "data/sfx/", channel : 2}
 	 */
 };
