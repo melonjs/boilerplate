265644d6bee6c7029fdb5bb6c8716294d4cd677b
Jason Oster <jay@pubnub.com>
2013-11-20 12:42:51 -0800

    Do not clobber game.HUD

--- js/game.js
+++ js/game.js
@@ -39,10 +39,10 @@ var game = {
 
 	// Run on game resources loaded.
 	"loaded" : function () {
-		 me.state.set(me.state.MENU, new game.TitleScreen());
+		me.state.set(me.state.MENU, new game.TitleScreen());
 		me.state.set(me.state.PLAY, new game.PlayScreen());
 
 		// Start the game.
-		me.state.change(me.state.PLAY);
+		me.state.change(me.state.MENU);
 	}
 };
--- js/screens/play.js
+++ js/screens/play.js
@@ -7,8 +7,8 @@ game.PlayScreen = me.ScreenObject.extend({
 		game.data.score = 0;
 
 		// add our HUD to the game world
-		game.HUD = new game.HUD.Container();
-		me.game.world.addChild(game.HUD);
+		this.HUD = new game.HUD.Container();
+		me.game.world.addChild(this.HUD);
 	},
 
 
@@ -17,6 +17,6 @@ game.PlayScreen = me.ScreenObject.extend({
 	 */
 	onDestroyEvent: function() {
 		// remove the HUD from the game world
-		me.game.world.removeChild(game.HUD);
+		me.game.world.removeChild(this.HUD);
 	}
 });
