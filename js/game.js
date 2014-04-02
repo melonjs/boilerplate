bf0243816dbb2f2c5d5eeca98755a05fe696831a
Ellison Leão <ellisonleao@gmail.com>
2014-04-02 13:01:29 -0300

    update game.js

--- js/game.js
+++ js/game.js
@@ -1,48 +1,33 @@
-
-/* Game namespace */
 var game = {
+  data: {
+    score: 0
+  },
 
-	// an object where to store game information
-	data : {
-		// score
-		score : 0
-	},
-	
-	
-	// Run on page load.
-	"onload" : function () {
-	// Initialize the video.
-	if (!me.video.init("screen", 480, 320, true, 'auto')) {
-		alert("Your browser does not support HTML5 canvas.");
-		return;
-	}
+  "onload": function() {
+    if (!me.video.init("screen", 800, 600, true, 'auto')) {
+      alert("Your browser does not support HTML5 canvas.");
+      return;
+    }
+    /*
+    // add "#debug" to the URL to enable the debug Panel
+    if (document.location.hash === "#debug") {
+      window.onReady(function () {
+        me.plugin.register.defer(this, debugPanel, "debug");
+      });
+    }
+    // not working on 1.0
+    */
+    me.audio.init("mp3,ogg");
+    me.loader.onload = this.loaded.bind(this);
+    me.loader.preload(game.resources);
+    me.state.change(me.state.LOADING);
+  },
 
-	// add "#debug" to the URL to enable the debug Panel
-	if (document.location.hash === "#debug") {
-		window.onReady(function () {
-			me.plugin.register.defer(debugPanel, "debug");
-		});
-	}
+  "loaded": function() {
+    me.state.set(me.state.PLAY, new game.PlayScreen());
 
-	// Initialize the audio.
-	me.audio.init("mp3,ogg");
-
-	// Set a callback to run when loading is complete.
-	me.loader.onload = this.loaded.bind(this);
-
-	// Load the resources.
-	me.loader.preload(game.resources);
-
-	// Initialize melonJS and display a loading screen.
-	me.state.change(me.state.LOADING);
-},
-
-	// Run on game resources loaded.
-	"loaded" : function () {
-		me.state.set(me.state.MENU, new game.TitleScreen());
-		me.state.set(me.state.PLAY, new game.PlayScreen());
-
-		// Start the game.
-		me.state.change(me.state.MENU);
-	}
+    // in melonJS 1.0.0, viewport size is set to Infinity by default
+    me.game.viewport.setBounds(0, 0, 800, 600);
+    me.state.change(me.state.PLAY);
+  }
 };
