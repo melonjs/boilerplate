09f8cda99fdd07c02c6ed7324c81efab0465eba6
Olivier Biot <olivier.biot@me.com>
2015-05-09 09:50:03 +0800

    updated with melonJS 2.1.0

--- js/game.js
+++ js/game.js
@@ -12,7 +12,7 @@ var game = {
     // Run on page load.
     "onload" : function () {
     // Initialize the video.
-    if (!me.video.init("screen",  me.video.CANVAS, 960, 640, true, 'auto')) {
+    if (!me.video.init(960, 640, {wrapper : "screen", scale : "auto"})) {
         alert("Your browser does not support HTML5 canvas.");
         return;
     }
