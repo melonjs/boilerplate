38c46969d4f537d15a92f65ced190a47bdc1f300
Olivier Biot <olivier.biot@me.com>
2015-05-20 12:41:29 +0800

    fixed the conditional debugPanel loading in cocoonJS
    Use the pre-hashed URL parameters, as `document.location.hash.match` is
    undefined under cocoonJS

--- js/game.js
+++ js/game.js
@@ -18,7 +18,7 @@ var game = {
     }
 
     // add "#debug" to the URL to enable the debug Panel
-    if (document.location.hash === "#debug") {
+    if (me.game.HASH.debug === true) {
         window.onReady(function () {
             me.plugin.register.defer(this, me.debug.Panel, "debug", me.input.KEY.V);
         });
