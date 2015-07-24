beafef3865e6217f10f5c9898a8616b953e6325c
Olivier Biot <olivier.biot@me.com>
2015-07-24 17:15:55 +0800

    fixed indentation

--- js/game.js
+++ js/game.js
@@ -11,31 +11,31 @@ var game = {
 
     // Run on page load.
     "onload" : function () {
-    // Initialize the video.
-    if (!me.video.init(960, 640, {wrapper : "screen", scale : "auto"})) {
-        alert("Your browser does not support HTML5 canvas.");
-        return;
-    }
+        // Initialize the video.
+        if (!me.video.init(960, 640, {wrapper : "screen", scale : "auto"})) {
+            alert("Your browser does not support HTML5 canvas.");
+            return;
+        }
 
-    // add "#debug" to the URL to enable the debug Panel
-    if (me.game.HASH.debug === true) {
-        window.onReady(function () {
-            me.plugin.register.defer(this, me.debug.Panel, "debug", me.input.KEY.V);
-        });
-    }
+        // add "#debug" to the URL to enable the debug Panel
+        if (me.game.HASH.debug === true) {
+            window.onReady(function () {
+                me.plugin.register.defer(this, me.debug.Panel, "debug", me.input.KEY.V);
+            });
+        }
 
-    // Initialize the audio.
-    me.audio.init("mp3,ogg");
+        // Initialize the audio.
+        me.audio.init("mp3,ogg");
 
-    // Set a callback to run when loading is complete.
-    me.loader.onload = this.loaded.bind(this);
+        // Set a callback to run when loading is complete.
+        me.loader.onload = this.loaded.bind(this);
 
-    // Load the resources.
-    me.loader.preload(game.resources);
+        // Load the resources.
+        me.loader.preload(game.resources);
 
-    // Initialize melonJS and display a loading screen.
-    me.state.change(me.state.LOADING);
-},
+        // Initialize melonJS and display a loading screen.
+        me.state.change(me.state.LOADING);
+    },
 
     // Run on game resources loaded.
     "loaded" : function () {
