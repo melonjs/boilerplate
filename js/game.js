32ffc637bd1586d9307b03a3ed55afeca2ee8421
Olivier Biot <olivier.biot@me.com>
2016-03-08 16:40:20 +0800

    this is not required anymore are plugin registration is now automatic

--- js/game.js
+++ js/game.js
@@ -17,13 +17,6 @@ var game = {
             return;
         }
 
-        // add "#debug" to the URL to enable the debug Panel
-        if (me.game.HASH.debug === true) {
-            window.onReady(function () {
-                me.plugin.register.defer(this, me.debug.Panel, "debug", me.input.KEY.V);
-            });
-        }
-
         // Initialize the audio.
         me.audio.init("mp3,ogg");
 
