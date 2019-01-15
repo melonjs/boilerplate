414e6d26fd9a753766c51f2753c898ece811cd41
TsaEIT <44852537+TsaEIT@users.noreply.github.com>
2019-01-15 17:05:26 -0500

    Update entities.js

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -35,12 +35,10 @@ game.PlayerEntity = me.Entity.extend({
     update : function (dt) {
         if (game.data.frozen) {
             moving = true;
-            // flip the sprite on horizontal axis
-            this.renderable.flipY(true);
 
             // update the entity velocity
             if (this.distanceTo(me.game.world.children.find((e)=>{return e.name == 'stopEntity'})) >= 9) { //Return here
-                this.body.vel.y = -0.5 * me.timer.tick;
+                this.body.vel.y = -0.3 * me.timer.tick;
             } else {
                 moving = false;
             }
@@ -66,16 +64,12 @@ game.PlayerEntity = me.Entity.extend({
             }
             if (me.input.isKeyPressed('up')) {
                 moving = true;
-                // flip the sprite on horizontal axis
-                this.renderable.flipY(true);
 
                 // update the entity velocity
                 this.body.vel.y -= this.body.accel.y * me.timer.tick;
                 }
             else if (me.input.isKeyPressed('down')) {
                 moving = true;
-                // unflip the sprite
-                this.renderable.flipY(false);
 
                 // update the entity velocity
                 this.body.vel.y += this.body.accel.y * me.timer.tick;
@@ -123,7 +117,6 @@ game.KeyEntity = me.CollectableEntity.extend({
   init: function (x, y, settings) {
     // call the parent constructor
     this._super(me.CollectableEntity, 'init', [x, y , settings]);
-
   },
 
   // this function is called by the engine, when
@@ -149,7 +142,7 @@ game.unhideEntity = me.Entity.extend({
   init: function (x, y, settings) {
     // call the parent constructor
     this._super(me.CollectableEntity, 'init', [x, y , settings]);
-    me.game.world.children.find((e)=>{return e.name == 'Shadow'}).alpha = 0.8;
+    me.game.world.children.find((e)=>{return e.name == 'Shadow'}).alpha = 0.9;
 
   },
 
@@ -212,7 +205,7 @@ game.vampireEntity = me.Entity.extend({
     this.renderable.addAnimation("normal",  [0]);
     this.renderable.setCurrentAnimation("normal");
   },
-
+  
   // this function is called by the engine, when
   // an object is touched by something (here collected)
   onCollision : function (response, other) {
@@ -235,3 +228,97 @@ game.stopEntity = me.Entity.extend({
     return false
   }
 });
+
+/**
+ * an enemy Entity
+ */
+ game.EnemyEntity = me.Sprite.extend(
+ {
+     init: function (x, y, settings)
+     {
+         // save the area size as defined in Tiled
+         var width = settings.width;
+
+         // define this here instead of tiled
+         settings.image = "wheelie_right";
+
+         // adjust the size setting information to match the sprite size
+         // so that the entity object is created with the right size
+         settings.framewidth = settings.width = 64;
+         settings.frameheight = settings.height = 64;
+
+         // call the parent constructor
+         this._super(me.Sprite, 'init', [x, y , settings]);
+
+         // add a physic body
+         this.body = new me.Body(this);
+         // add a default collision shape
+         this.body.addShape(new me.Rect(0, 0, this.width, this.height));
+         // configure max speed and friction
+         this.body.setMaxVelocity(4, 6);
+         this.body.setFriction(0.4, 0);
+         // enable physic collision (off by default for basic me.Renderable)
+         this.isKinematic = false;
+
+         // set start/end position based on the initial area size
+         x = this.pos.x;
+         this.startX = x;
+         this.pos.x = this.endX = x + width - this.width;
+         //this.pos.x  = x + width - this.width;
+
+         // to remember which side we were walking
+         this.walkLeft = false;
+
+         // make it "alive"
+         this.alive = true;
+     },
+
+     // manage the enemy movement
+     update : function (dt)
+     {
+         if (this.alive)
+         {
+             if (this.walkLeft && this.pos.x <= this.startX)
+             {
+                 this.walkLeft = false;
+                 this.body.force.x = this.body.maxVel.x;
+             }
+             else if (!this.walkLeft && this.pos.x >= this.endX)
+             {
+                 this.walkLeft = true;
+                 this.body.force.x = -this.body.maxVel.x;
+             }
+
+             this.flipX(this.walkLeft);
+         }
+         else
+         {
+             this.body.force.x = 0;
+         }
+         // check & update movement
+         this.body.update(dt);
+
+         // handle collisions against other shapes
+         me.collision.check(this);
+
+         // return true if we moved or if the renderable was updated
+         return (this._super(me.Sprite, 'update', [dt]) || this.body.vel.x !== 0 || this.body.vel.y !== 0);
+     },
+
+     /**
+      * colision handler
+      * (called when colliding with other objects)
+      */
+     onCollision : function (response, other) {
+         if (response.b.body.collisionType !== me.collision.types.WORLD_SHAPE) {
+             // res.y >0 means touched by something on the bottom
+             // which mean at top position for this one
+             if (this.alive && (response.overlapV.y > 0) && response.a.body.falling) {
+                 this.renderable.flicker(750);
+             }
+             return false;
+         }
+         // Make all other objects solid
+         return true;
+     }
+ });
