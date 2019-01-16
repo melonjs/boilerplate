5d46007176b24a12c2a5d8aa62cf48936e17f069
TSAEit <tjm27@scasd.org>
2019-01-16 16:35:30 -0500

    Added tobecontinuedimg.tsx, so the thing would load.

---
+++ data/map/tobecontinuedimg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="tobecontinuedimg" tilewidth="8" tileheight="8" tilecount="180" columns="15">
+ <image source="../img/map/tobecontinuedimg.png" width="120" height="96"/>
+</tileset>
